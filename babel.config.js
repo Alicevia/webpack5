59024d1213e52c975f2de2de373ffe6b8886c297
Alicevia <ed-phoenix@qq.com>
2020-12-01 09:04:17 +0800

    asd

--- babel.config.js
+++ babel.config.js
@@ -12,11 +12,11 @@ module.exports = function (api) {
     ],
     [
       "@babel/preset-react",
-      {
-        "pragma": "dom", // default pragma is React.createElement
-        "pragmaFrag": "DomFrag", // default is React.Fragment
-        "throwIfNamespace": false // defaults to true
-      }
+      // {
+      //   "pragma": "dom", // default pragma is React.createElement
+      //   "pragmaFrag": "DomFrag", // default is React.Fragment
+      //   "throwIfNamespace": false // defaults to true
+      // }
     ]
   ];
   const plugins = [
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = <div>sdf</div>
 // const App = React.createElement('div', {
 //   className: 'test'
 // }, 'helloworld')
-
-ReactDOM.render(App, document.getElementById('root'))
-
+class App extends Component {
+  render() {
+    return (<div>sdf</div>)
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
 
 
 
