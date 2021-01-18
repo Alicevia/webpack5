43f8f91dcadb344413b77e813a95174469b86308
Alicevia <ed-phoenix@qq.com>
2021-01-18 15:02:06 +0800

    'test-stash'

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
-let a ={
-  "semi":false,//是否加分号
-  "singleQuote":true,//字符串用单引号
-  "arrowParens":"always",//箭头函数参数只有一个的时候是否使用括号 avoid单个参数不使用
-  "trailingComma":"all",//键值对最后一个是否加逗号
+let a = {
+  "semi": false,//是否加分号
+  "singleQuote": true,//字符串用单引号
+  "arrowParens": "always",//箭头函数参数只有一个的时候是否使用括号 avoid单个参数不使用
+  "trailingComma": "all",//键值对最后一个是否加逗号.
 }
